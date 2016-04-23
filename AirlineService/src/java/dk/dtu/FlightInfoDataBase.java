c80081dc00bad1c58d82ee1e63f9fd5726b0adee
GreatCodeNinja <djonifrank@gmail.com>
2016-04-23 11:17:19 +0200

    DB123

--- AirlineService/src/java/dk/dtu/FlightInfoDataBase.java
+++ AirlineService/src/java/dk/dtu/FlightInfoDataBase.java
@@ -41,17 +41,21 @@ public class FlightInfoDataBase {
     GregorianCalendar ed5 = new GregorianCalendar(2015, 9, 7);
     XMLGregorianCalendar endDate5 = DatatypeFactory.newInstance().newXMLGregorianCalendar(ed5);
     
-    FlightInfo flight_1 = new FlightInfo("Copenhagen", "Berlin", "B12341", 
-            "LameDuck", "SAS", startDate1, endDate1, 50);
-    FlightInfo flight_2 = new FlightInfo("Berlin", "Amsterdam", "B12342", 
-            "LameDuck", "Ryanair", startDate2, endDate2, 100);
-    FlightInfo flight_3 = new FlightInfo("Amsterdam", "Paris", "B12343", 
-            "LameDuck", "SAS", startDate3, endDate3, 88);
-    FlightInfo flight_4 = new FlightInfo("Paris", "Madrid", "B12344", 
-            "LameDuck", "Ryanair", startDate4, endDate4, 66);
+    FlightInfo flight_1;
+    FlightInfo flight_2;
+    FlightInfo flight_3;
+    FlightInfo flight_4;
     FlightInfo flight_5;
 
     public FlightInfoDataBase() throws DatatypeConfigurationException {
+        this.flight_1 = new FlightInfo("Copenhagen", "Berlin", "B12341", 
+                "LameDuck", "SAS", startDate1, endDate1, 50);
+        this.flight_2 = new FlightInfo("Berlin", "Amsterdam", "B12342", 
+                "LameDuck", "Ryanair", startDate2, endDate2, 100);
+        this.flight_3 = new FlightInfo("Amsterdam", "Paris", "B12343", 
+                "LameDuck", "SAS", startDate3, endDate3, 88);
+        this.flight_4 = new FlightInfo("Paris", "Madrid", "B12344", 
+                "LameDuck", "Ryanair", startDate4, endDate4, 66);        
         this.flight_5 = new FlightInfo("Madrid", "Copenhagen", "B12345", 
                 "LameDuck", "SAS", startDate5, endDate5, 70);
     }
