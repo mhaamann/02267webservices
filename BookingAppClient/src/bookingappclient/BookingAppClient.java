11cf2eba80afea265031e465c4aa17ab92d0f95c
Matthias Haamann <mh@churchdesk.com>
2016-05-01 19:19:59 +0200

    it works

--- BookingAppClient/src/bookingappclient/BookingAppClient.java
+++ BookingAppClient/src/bookingappclient/BookingAppClient.java
@@ -19,14 +19,21 @@ public class BookingAppClient {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Starting");
-        String createItinerary = createItinerary("5");
+        //String createItinerary = createItinerary("5");
 
-        System.out.println(createItinerary);
+        //System.out.println(createItinerary);
         
         GetHotelsResponse hotels = getHotels("Copenhagen", "2016-01-01", "2016-01-30", "5");
         
         for (Hotel hotel : hotels.getReturn()) {
             System.out.println(hotel.city);
+        }
+        
+        addHotel("7", "5");
+        HotelList hotelList = addHotel("12", "5");
+        for (HotelReservation reservation : hotelList.getReservation()) {
+            System.out.print(reservation.bookingNumber + " - Status:");
+            System.out.println(reservation.status);
         }
         
     }
@@ -41,6 +48,12 @@ public class BookingAppClient {
         bookingappclient.Service1 service = new bookingappclient.Service1();
         bookingappclient.BookingServicePortType port = service.getBookingServicePortTypeBindingPort();
         return port.getHotels(city, arrival, departure, itineraryId);
+    }
+
+    private static HotelList addHotel(java.lang.String bookingNumber, java.lang.String itineraryId) {
+        bookingappclient.Service1 service = new bookingappclient.Service1();
+        bookingappclient.BookingServicePortType port = service.getBookingServicePortTypeBindingPort();
+        return port.addHotel(bookingNumber, itineraryId);
     }
 
     
