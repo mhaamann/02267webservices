fe0ca418822704ad1017fc4770bd2140d2cf67b6
Matthias Haamann <mh@churchdesk.com>
2016-05-01 11:33:54 +0200

    Fixed wsdl

--- BookingAppClient/src/bookingappclient/BookingAppClient.java
+++ BookingAppClient/src/bookingappclient/BookingAppClient.java
@@ -16,7 +16,7 @@ public class BookingAppClient {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        
+        System.out.println("Starting");
         String createItinerary = createItinerary();
         
         System.out.println(createItinerary);
