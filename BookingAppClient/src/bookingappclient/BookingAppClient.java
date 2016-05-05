a16aaa71f8e1a815f531eb7b0c16a23f85a2a7f0
Matthias Haamann <mh@churchdesk.com>
2016-05-05 16:26:13 +0200

    gem2

--- BookingAppClient/src/bookingappclient/BookingAppClient.java
+++ BookingAppClient/src/bookingappclient/BookingAppClient.java
@@ -5,7 +5,18 @@
  */
 package bookingappclient;
 
+import ExternalBookingService.GetHotelsResponse;
+import ExternalBookingService.Hotel;
+import ExternalBookingService.HotelList;
+import ExternalBookingService.HotelReservation;
 import java.util.Arrays;
+import javax.xml.namespace.QName;
+import javax.xml.transform.Source;
+import javax.xml.ws.Dispatch;
+import javax.xml.transform.stream.StreamSource;
+import javax.xml.ws.Service;
+import java.io.StringReader;
+import java.math.BigInteger;
 
 /**
  *
@@ -19,25 +30,53 @@ public class BookingAppClient {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Starting");
-        String createItinerary = createItinerary("5");
+        String itinerary = "6";
+        String createItinerary = createItinerary(itinerary);
 
         System.out.println(createItinerary);
         
-        GetHotelsResponse hotels = getHotels("Copenhagen", "2016-01-01", "2016-01-30", "5");
+        GetHotelsResponse hotels = getHotels("Copenhagen", "2016-01-01", "2016-01-30", itinerary);
         
         for (Hotel hotel : hotels.getReturn()) {
-            System.out.println(hotel.city);
+            System.out.println(hotel.getCity());
         }
         
-        addHotel("7", "5");
-        HotelList hotelList = addHotel("12", "5");
+        addHotel("7", itinerary);
+        HotelList hotelList = addHotel("12", itinerary);
         for (HotelReservation reservation : hotelList.getReservation()) {
-            System.out.print(reservation.bookingNumber + " - Status:");
-            System.out.println(reservation.status);
+            System.out.print(reservation.getBookingNumber() + " - Status:");
+            System.out.println(reservation.getStatus());
         }
+        
+        bookItinerary(itinerary, "Anne Strandberg", BigInteger.valueOf(50408816), BigInteger.valueOf(5), BigInteger.valueOf(9));
         
     }
 
+    private static HotelList addHotel(java.lang.String bookingNumber, java.lang.String itineraryId) {
+        ExternalBookingService.Service1 service = new ExternalBookingService.Service1();
+        ExternalBookingService.BookingServicePortType port = service.getBookingServicePortTypeBindingPort();
+        return port.addHotel(bookingNumber, itineraryId);
+    }
+
+    private static HotelList bookItinerary(java.lang.String itineraryId, java.lang.String name, java.math.BigInteger number, java.math.BigInteger month, java.math.BigInteger year) {
+        ExternalBookingService.Service1 service = new ExternalBookingService.Service1();
+        ExternalBookingService.BookingServicePortType port = service.getBookingServicePortTypeBindingPort();
+        return port.bookItinerary(itineraryId, name, number, month, year);
+    }
+
+    private static String createItinerary(java.lang.String itineraryId) {
+        ExternalBookingService.Service1 service = new ExternalBookingService.Service1();
+        ExternalBookingService.BookingServicePortType port = service.getBookingServicePortTypeBindingPort();
+        return port.createItinerary(itineraryId);
+    }
+
+    private static GetHotelsResponse getHotels(java.lang.String city, java.lang.String arrival, java.lang.String departure, java.lang.String itineraryId) {
+        ExternalBookingService.Service1 service = new ExternalBookingService.Service1();
+        ExternalBookingService.BookingServicePortType port = service.getBookingServicePortTypeBindingPort();
+        return port.getHotels(city, arrival, departure, itineraryId);
+    }
+
+   
     
 
     
