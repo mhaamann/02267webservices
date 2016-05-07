6a61ecc8322b47dca0d1b7513f986602d2225343
Matthias Haamann <mh@churchdesk.com>
2016-05-07 16:47:26 +0200

    Removed bank service since it broke BookingAppClient

--- AirlineService/src/java/dk/dtu/AirlineWebService.java
+++ AirlineService/src/java/dk/dtu/AirlineWebService.java
@@ -5,9 +5,7 @@
  */
 package dk.dtu;
 
-import dk.dtu.imm.fastmoney.BankService;
-import dk.dtu.imm.fastmoney.CreditCardFaultMessage;
-import dk.dtu.imm.fastmoney.types.AccountType;
+
 import javax.jws.WebService;
 import javax.jws.WebMethod;
 import javax.jws.WebParam;
@@ -22,15 +20,7 @@ import javax.xml.datatype.XMLGregorianCalendar;
 @WebService(serviceName = "AirlineWebService")
 public class AirlineWebService {
 
-    //@WebServiceRef(wsdlLocation = "WEB-INF/wsdl/fastmoney.imm.dtu.dk_8080/BankService.wsdl")
-    private BankService service;
     FlightInfoDataBase flightDB;
-    private static final AccountType account = new AccountType();
-
-    static {
-        account.setName("CheapBird");
-        account.setNumber("50208813");
-    }
 
     public AirlineWebService() throws DatatypeConfigurationException {
         this.flightDB = new FlightInfoDataBase();
@@ -72,31 +62,21 @@ public class AirlineWebService {
     @WebMethod(operationName = "cancelFlight")
     public boolean cancelFlight(@WebParam(name = "bookingNumber") String bookingNumber,
             @WebParam(name = "price") int price,
-            @WebParam(name = "creditCard") dk.dtu.imm.fastmoney.types.CreditCardInfoType creditCard) {
+            @WebParam(name = "year") int year,
+            @WebParam(name = "month") int month,
+            @WebParam(name = "number") int number,
+            @WebParam(name = "name") String name) {
         for (FlightInfo flight : flightDB.flightList) {
             if (flight.bookingNumber.equals(bookingNumber)) {
-                try {
-                    refundCreditCard(1, creditCard, flight.price, account);
-                    return true;
-                } catch (CreditCardFaultMessage e) {
-                }//end try/catch
+                
+                // refundCreditCard(1, creditCard, flight.price, account);
+                return true;
+                
             } else {
                 return false;
             }//end if/else
         }//end for
         return false;
-    }
-
-    private boolean chargeCreditCard(int group, dk.dtu.imm.fastmoney.types.CreditCardInfoType creditCardInfo,
-            int amount, dk.dtu.imm.fastmoney.types.AccountType account) throws CreditCardFaultMessage {
-        dk.dtu.imm.fastmoney.BankPortType port = service.getBankPort();
-        return port.chargeCreditCard(group, creditCardInfo, amount, account);
-    }
-
-    private boolean refundCreditCard(int group, dk.dtu.imm.fastmoney.types.CreditCardInfoType creditCardInfo,
-            int amount, dk.dtu.imm.fastmoney.types.AccountType account) throws CreditCardFaultMessage {
-        dk.dtu.imm.fastmoney.BankPortType port = service.getBankPort();
-        return port.refundCreditCard(group, creditCardInfo, amount, account);
     }
 
 }
